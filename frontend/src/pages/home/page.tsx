e5ef8862bf27e230954bf4b32aea1c76b5e3d2e1
Khan Tran <kcoder282@gmail.com>
2025-10-27 10:10:39 +0700

    push

--- frontend/src/pages/home/page.tsx
+++ frontend/src/pages/home/page.tsx
@@ -312,7 +312,7 @@ export default function Page() {
                     valueLabelDisplay="auto"
                     sx={{ height: 12 }}
                     min={0}
-                    max={20}
+                    max={10}
                     step={1}
                     {...field}
                   />
